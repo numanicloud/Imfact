a9c4998c7ae18bc098a553de3550493a6b2fca06
NumAniCloud <num.ani.cloud@gmail.com>
2022-12-13 23:48:08 +0900

    ラッパークラスを作成

---
+++ Dev/Imfact/Steps/Filter/Wrappers/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿namespace Imfact.Steps.Filter.Wrappers;
+
+internal interface IFactoryClassWrapper
+{
+}
+
+internal interface IResolverWrapper
+{
+}
+
+internal interface IReturnTypeWrapper
+{
+}
+
+internal interface IAttributeWrapper
+{
+}
+
+internal interface IBaseFactoryWrapper
+{
+}
+
+internal interface IResolutionFactoryWrapper
+{
+}
---
+++ Dev/Imfact/Steps/Filter/Wrappers/SymbolWrapper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Imfact.Steps.Filter.Wrappers;
+
+internal class FactorySymbolWrapper : IFactoryClassWrapper
+{
+    public required INamedTypeSymbol Symbol { get; init; }
+}
+
+internal class ResolverSymbolWrapper : IResolverWrapper
+{
+    public required IMethodSymbol Symbol { get; init; }
+}
+
+internal class ReturnTypeSymbolWrapper : IReturnTypeWrapper
+{
+    public required INamedTypeSymbol Symbol { get; init; }
+}
+
+internal class AttributeSymbolWrapper : IAttributeWrapper
+{
+    public required AttributeData Data { get; init; }
+    public required INamedTypeSymbol Symbol { get; init; }
+}
+
+internal class BaseFactorySymbolWrapper : IBaseFactoryWrapper
+{
+    public required INamedTypeSymbol Symbol { get; init; }
+}
+
+internal class ResolutionSymbolWrapper : IResolutionFactoryWrapper
+{
+    public required INamedTypeSymbol Symbol { get; init; }
+}
